6f497b68743c80702094159d4bc34feb9e4c66b1
dscardona <89112552+dscardona@users.noreply.github.com>
2023-05-12 15:59:25 +0000

    Make to do list of tasks

---
+++ main.py
@@ -0,0 +1,7 @@
+# Welcome user to game
+# I'm thinking of a number between 1 and 100: randomly select a target number, save as target
+# Select level: hard or easy. if hard: 5 attempts if easy 10 attemps
+# Ask player to make a guess, compare to target, print clue, take input again (loop)
+# if tries expire: you lose
+# if guess = target before tries expire: you win
+#play again?
