a8c37fc913c91da4343e0e4ecd9437b53b7a3cff
dscardona <89112552+dscardona@users.noreply.github.com>
2023-05-12 16:03:46 +0000

    Add welcome message and logo

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 # Welcome user to game
+from art import logo
+
+print(logo)
+print("Welcome to the number guessing game!")
 # I'm thinking of a number between 1 and 100: randomly select a target number, save as target
 # Select level: hard or easy. if hard: 5 attempts if easy 10 attemps
 # Ask player to make a guess, compare to target, print clue, take input again (loop)
