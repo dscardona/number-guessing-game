947fbae9055038c1e5215ffa996dfc876212fb9c
dscardona <89112552+dscardona@users.noreply.github.com>
2023-05-12 16:08:06 +0000

    Define target number with random module

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 # Welcome user to game
 from art import logo
+import random
 
 print(logo)
 print("Welcome to the number guessing game!")
 # I'm thinking of a number between 1 and 100: randomly select a target number, save as target
+target = random.randint(1, 100)
+print(target)
+print("I'm thinking of a number between 1 and 100.")
+
 # Select level: hard or easy. if hard: 5 attempts if easy 10 attemps
 # Ask player to make a guess, compare to target, print clue, take input again (loop)
 # if tries expire: you lose
